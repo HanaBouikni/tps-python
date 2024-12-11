cd56175533089a48e9894ac3e13508de8a580f7d
HanaBouikni <155245299+HanaBouikni@users.noreply.github.com>
2024-12-11 12:41:55 +0100

    exo2

---
+++ exo2.py
@@ -0,0 +1,9 @@
+temp=int(input("please type in the temperature:"))
+
+if (temp<0):
+    print("it's freezing !")
+if(temp<10):
+    print("it's cold !")
+if(temp<20):    
+    print("it's cool !")
+print("Stay Safe!")
