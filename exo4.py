8cb791c18b966b0057eeb80a65de07811581ac5b
HanaBouikni <155245299+HanaBouikni@users.noreply.github.com>
2024-12-11 13:23:28 +0100

    exo4

---
+++ exo4.py
@@ -0,0 +1,9 @@
+age1=int(input("Please type in the age of the first pesrson: "))
+age2=int(input("Please type in the age of the second pesrson:"))
+
+if(age1>age2):
+    print("the older age is :",age1)
+elif(age1<age2):
+    print("the older age is :",age2)
+else:
+    print("Both people are the same age!")
