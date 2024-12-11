fe05b61473595760ec4b1bb0a92563c27ccfb855
HanaBouikni <155245299+HanaBouikni@users.noreply.github.com>
2024-12-11 12:23:24 +0100

    exo1

---
+++ exo1.py
@@ -0,0 +1,9 @@
+name=str(input("please enter you name: "))
+
+if (name!='VIP'):
+   ticket=int(input("how many tickets would you like to  buy ? "))
+   ticket = ticket*15.50
+   print("The total cost is ",ticket) 
+   print("Enjoy the show for free !")
+else:
+   print("Enjoy the show for free !")
